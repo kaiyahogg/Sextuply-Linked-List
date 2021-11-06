4d00c3c6f7645c8041b7e3cd35ee490962e50663
liamm <liammullen0@gmail.com>
2021-11-06 19:28:19 -0400

    fuck

--- Node.py
+++ Node.py
@@ -1,13 +1,11 @@
 class Node:
     # Node state variables
-    _data = None
-    next_up = None
-    next_down = None
-    next_left = None
-    next_right = None
-    next_back = None
-    next_forward = None
-    
     def __init__(self, data):
         self._data = data
+        self._up = None
+        self._down = None
+        self._left = None
+        self._right = None
+        self._back = None
+        self._forward = None
     
--- sextuplylinkedlist.py
+++ sextuplylinkedlist.py
@@ -1,5 +1,11 @@
+from Node import Node
+
+
 class SextuplyLinkedList:
-    _head = None
+    _head: Node = None
+    _RowIterator: Node = None
+    _ColIterator: Node = None
+    _DepIterator: Node = None
     _height = _width = _depth = 0
 
     # Linked list functions
@@ -7,7 +13,44 @@ class SextuplyLinkedList:
     # Insert/Remove
     # Iterator
 
-    def __init__(self, node):
-        self._head = node
-        _width = 1
+    def __init__(self, height, width, depth):
+        self._height = height
+        self._width = width
+        self._depth = depth
 
+        self._head = Node("head")
+        self._RowIterator = self._ColIterator = self._DepIterator = self._head
+
+        #grid for each layer
+        createHyperGrid(dz, _width, _depth)
+
+
+    def createHyperGrid(self, width, depth):
+        for dz in height:
+            for dy in depth:
+                for dx in width:
+
+                    if(dy==0):
+                        self._RowIterator._right = Node()
+                    else:
+                        self._RowIterator._right = self._RowIterator._back._right._forward
+
+                    self._RowIterator._forward = Node()
+                    self._RowIterator._up = Node()
+
+                    #doubly link
+                    self._RowIterator._right._left = self._RowIterator
+                    self._RowIterator._forward._back = self._RowIterator
+                    self._RowIterator._up._down = self._RowIterator
+
+                    if (dx == 0):
+                        self._ColIterator = self._RowIterator._forward
+                        self._DepIterator = self._ColIterator._up
+
+                    self._RowIterator = self._RowIterator._right
+
+                self._RowIterator = self._ColIterator
+
+            self._RowIterator = self._DepIterator
+
+        return
