a5960bd1cc00f3cc8e9fc8f3e1199e55cd63ce8f
Kaiya Hogg <kaiya.hogg@gmail.com>
2021-11-06 16:34:59 -0400

    added Node class for the linked list

---
+++ Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, id, next_up, next_down, next_left, next_right, next_back, next_forward):
+        self.id = id
+        self.next_up = next_up
+        self.next_down = next_down
+        self.next_left = next_left
+        self.next_right = next_right
+        self.next_back = next_back
+        self.next_forward = next_forward
+        
--- sextuplylinkedlist.py
+++ sextuplylinkedlist.py
@@ -1,1 +1,11 @@
+class SextuplyLinkedList:
 
+    # Linked list functions
+    # Insert/Remove
+    # Iterator
+    # Apply
+
+
+
+    def __init__(self, node):
+        self.node = node
